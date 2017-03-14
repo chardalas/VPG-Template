2a593ee41e83631fc11b4579d14dc7a4c0269143
manChard <emmhard@gmail.com>
2017-03-14 17:05:46 +0100

    Initial Commit of vpg template
    I'm committing all files to master but the clean up will be done to the branch latter on.

---
+++ page_theme.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Application\Theme\Urbanic;
+
+use Concrete\Core\Page\Theme\Theme;
+
+/**
+* 
+*/ 
+class PageTheme extends Theme
+{
+	protected $pThemeGridFrameworkHandle = 'bootstrap3';
+
+	//public function getThemeBlockClasses() {
+ 		//return array(
+/// 			'image' => array('img-manos')
+	//	);
+	//}
+}
